6020a29cb4f20ab3160ecf29fdc5ed7f21c86e52
Andrew Cring <amcring1@gmail.com>
2022-04-21 15:41:19 -0700

    section 2 of the exercise

---
+++ data_presenter.py
@@ -0,0 +1,21 @@
+data = open('./CupcakeInvoices.csv')
+
+for row in data:
+    print(row)
+
+for row in data:
+    value = row.split(',')
+    print(value[2])
+
+for row in data:
+    values = row.split(',')
+    total = int(values[3]) * float(values[4])
+    print(total)
+
+total = 0
+for row in data:
+  values = row.split(',')
+  total = total + (int(values[3]) * float(values[4]))
+print(total)
+
+data.close()
