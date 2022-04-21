ac76adb56c72d856e9ed2b3d00bc8b52cdcf66f7
Andrew Cring <amcring1@gmail.com>
2022-04-21 15:20:10 -0700

    The last questions for the beginner python exercises.

--- starter.py
+++ starter.py
@@ -28,23 +28,27 @@ else:
 
 # PROBLEM 6
 # Create an array called `colors` and set it equal to a list of at least five colors.
-
+colors = ['Green', 'Blue', 'Red', 'Yellow', 'Orange']
 
 # Problem 7
 # Using bracket syntax, print out the last item in your colors array.
-
+print(colors[4])
 
 # For problems 8-9, use the following line of code:
 numbers = [1,2,3,4,5,6,7,8,9,10]
 
 # Problem 8
 # Use a for-in loop to iterate over the `numbers` array and print each number.
-
+for number in numbers:
+    print(number)
 
 # Problem 9
 # Create an empty array called `even_numbers`.
 # Use a for-in loop to iterate over the `numbers` array, and if a number is even, add  it to the `even_numbers` array.
-
+even_numbers = []
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
 
 # Problem 10
 # Do not edit the code below.
@@ -52,12 +56,25 @@ score = 74
 # Do not edit the code above.
 
 # Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console-log an 'A', between 80 and 89, console-log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console-log an 'F'.
-
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
 
 # Problem 11
 # Create a variable called 'changeMyMind' and set it equal to true. 
 # Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
-
+changeMyMind = True
+if changeMyMind == True:
+    changeMyMind = False
+else:
+    changeMyMind = True
 
 # ADVANCED
 
